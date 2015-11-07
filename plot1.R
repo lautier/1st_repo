4bd9b1414a2265b2f95e07c8775719a35870d636
lautier <m.jakubowska@ymail.com>
2015-11-07 13:57:21 +0100

    Exploratory Analysis Course Project 1

--- plot1.R
+++ plot1.R
@@ -1,7 +1,10 @@
-
 consumption <- read.csv("household_power_consumption.txt", sep=";")
 
 sub.con<-subset(consumption, Date=="1/2/2007"| Date=="2/2/2007")
 sub.con$Global_active_power<- as.numeric(as.character(sub.con$Global_active_power))
 
-hist(dd$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+quartz()
+hist(sub.con$Global_active_power, xlab="Global Active Power (kilowatts)", main="Global Active Power", col="red")
+
+quartz.save("plot1.png", type="png", device=dev.cur(), canvas="white")
+dev.off
--- plot2.R
+++ plot2.R
@@ -1,6 +1,13 @@
-consumption <- read.csv("household_power_consumption.txt", sep=";")
 
+consumption <- read.csv("household_power_consumption.txt", sep=";")
+library(lubridate)
 sub.con<-subset(consumption, Date=="1/2/2007"| Date=="2/2/2007")
 sub.con$Global_active_power<- as.numeric(as.character(sub.con$Global_active_power))
 sub.con$Date2<- dmy_hms(paste(sub.con$Date, sub.con$Time))
+
+quartz()
+
 plot(sub.con$Date2, sub.con$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+quartz.save("plot2.png", type="png", device=dev.cur(), canvas="white")
+dev.off
