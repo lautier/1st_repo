78bf20170c4df2f79ea8605979e6ad72c1cb8a69
lautier <m.jakubowska@ymail.com>
2015-11-07 19:39:57 +0100

    Exploratory Analysis Course Project 1

--- plot3.R
+++ plot3.R
@@ -1,14 +1,18 @@
 consumption <- read.csv("household_power_consumption.txt", sep=";")
-
+library(lubridate)
 sub.con<-subset(consumption, Date=="1/2/2007"| Date=="2/2/2007")
 sub.con$Date2<- dmy_hms(paste(sub.con$Date, sub.con$Time))
 sub.con$Sub_metering_1<- as.numeric(as.character(sub.con$Sub_metering_1))
 sub.con$Sub_metering_2<- as.numeric(as.character(sub.con$Sub_metering_2))
 sub.con$Sub_metering_3<- as.numeric(as.character(sub.con$Sub_metering_3))
 
+quartz()
 
-plot(sub.con$Date2, sub.con$Sub_metering_1, type="l", col="black")
+plot(sub.con$Date2, sub.con$Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab="")
 lines(sub.con$Date2, sub.con$Sub_metering_2, type="l", col="red")
 lines(sub.con$Date2, sub.con$Sub_metering_3, type="l", col="blue")
 
-legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+quartz.save("plot3.png", type="png", device=dev.cur(), canvas="white")
+dev.off
--- plot4.R
+++ plot4.R
@@ -1,5 +1,5 @@
 consumption <- read.csv("household_power_consumption.txt", sep=";")
-
+library(lubridate)
 sub.con<-subset(consumption, Date=="1/2/2007"| Date=="2/2/2007")
 sub.con$Date2<- dmy_hms(paste(sub.con$Date, sub.con$Time))
 sub.con$Sub_metering_1<- as.numeric(as.character(sub.con$Sub_metering_1))
@@ -12,7 +12,7 @@ quartz()
 
 par(mfrow=c(2,2))
 
-plot(sub.con$Date2, sub.con$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+plot(sub.con$Date2, sub.con$Global_active_power, type="l", ylab="Global Active Power", xlab="")
 
 plot(sub.con$Date2, sub.con$Voltage, type="l", ylab="Voltage", xlab="datetime")
 
@@ -20,10 +20,10 @@ plot(sub.con$Date2, sub.con$Sub_metering_1, type="l", col="black", ylab="Energy 
 lines(sub.con$Date2, sub.con$Sub_metering_2, type="l", col="red")
 lines(sub.con$Date2, sub.con$Sub_metering_3, type="l", col="blue")
 
-legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2, bty="n")
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=1, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), box.col = "white")
 
 plot(sub.con$Date2, sub.con$Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime")
 
 
-dev.copy(png, "plot4.png")
+quartz.save("plot4.png", type="png", device=dev.cur(), canvas="white")
 dev.off
